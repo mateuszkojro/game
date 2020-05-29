91bc5ae215892bf3ff04d16584ba6cfafc34127f
mateuszkojro <mateusz.kojro@unilodz.eu>
2020-05-29 21:35:55 +0200

    player class

--- player.js
+++ player.js
@@ -1,12 +1,24 @@
 class player{
-    
-    position_x;
-    position_y;
-    scale;
+    constructor(_asset_path){
 
-    move(x, y);
-    scale(k );
-    draw();
+        this.asset = loadImage(_asset_path);
+        this.position_x = 0;
+        this.position_y = 0;
+        this.scale = 1;
+
+    }
+    move(x, y){
+        this.position_x += x * 50;
+        this.position_y += y * 50;
+    };
+    scale(k ){
+        this.scale *= k;
+    };
+    draw(){
+        scale(this.scale);
+        img(this.asset,this.position_x,this.position_y);
+        scale(1/this.scale);
+    };
 
 
 }
